414a93490766947e2f03b9e32848621e21b70163
Angle <abdukarimovtolib12@gmail.com>
2026-02-03 22:15:59 +0700

    #2 Added Config class and edited application config file

---
+++ Monolit/Infrastructure/AppConfig.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Copyright © 2026 Angle-Tolib, All Rights Reserved.
+ */
+
+namespace Monolit.Infrastructure
+{
+    public class AppConfig
+    {
+        public TinyMCE TinyMCE { get; set; } = new TinyMCE();
+        public Company Company { get; set; } = new Company();
+    }
+    public class TinyMCE
+    {
+        public string? APIKey { get; set; }
+    }
+
+    public class Company
+    {
+        public string? CompanyName { get; set; }
+        public string? CompanyPhone { get; set; }
+        public string? CompanyPhoneShort { get; set; }
+        public string? CompanyEmail { get; set; }
+        public string? CompanySupportEmail { get; set; }
+    }
+}
