1de0827054670798718fff9adda89597fd260f5c
Angle <135405684+Tolib-Angle@users.noreply.github.com>
2026-02-04 22:19:14 +0700

    #7 Edited content and added company info (#8)

---
+++ Monolit/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿/*
+    Copyright © 2026 Angle-Tolib, All Rights Reserved.
+ */
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Monolit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
