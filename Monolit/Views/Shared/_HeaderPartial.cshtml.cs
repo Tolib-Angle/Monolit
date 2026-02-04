15e9f06d0b6676565f601f8011ef46241da1ae08
Angle <abdukarimovtolib12@gmail.com>
2026-02-04 09:38:11 +0700

    #5 Added web application layout

---
+++ Monolit/Views/Shared/_HeaderPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Monolit.Views.Shared
+{
+    public class _HeaderPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Monolit/Views/Shared/_ScriptsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Monolit.Views.Shared
+{
+    public class _ScriptsPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
