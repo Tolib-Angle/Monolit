f9fa8349d067b90a3ffc5657ce094f2853f59fc9
Angle <abdukarimovtolib12@gmail.com>
2026-02-04 22:36:19 +0700

    #9 Configured application and program file

--- Monolit/Program.cs
+++ Monolit/Program.cs
@@ -2,18 +2,36 @@
     Copyright © 2026 Angle-Tolib, All Rights Reserved.
  */
 
+using Microsoft.Extensions.Configuration;
+using Monolit.Infrastructure;
+
 namespace Monolit
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-            app.Run();
+            IConfiguration configuration = configurationBuilder.Build();
+            AppConfig config = configuration.GetSection("Project").Get<AppConfig>()!;
+
+            builder.Services.AddControllersWithViews();
+
+            WebApplication app = builder.Build();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+            await app.RunAsync();
         }
     }
 }
